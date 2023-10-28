a85ff0086afcd157c039d57a1fc376c7dc5e562e
Asus <Asus@DESKTOP-5HP62ND>
2023-10-28 16:27:45 +0400

    Add project files.

---
+++ Ecommerce.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Core.DataAccess.EntityFramework;
+using Ecommerce.DataAccess.Abstract;
+using Ecommerce.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : EfRepositoryBase<Category, AppDbContext>, ICategoryDal
+    {
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ecommerce.Entities/Concrete/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entities.Concrete
+{
+    public class Order
+    {public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public decimal Price { get; set; }  
+        public decimal Quantity { get; set; }
+        public string Message { get; set; }
+
+    }
+}
